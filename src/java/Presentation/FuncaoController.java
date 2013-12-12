05d10d7fa959cd13728e72ab3f1c97b42df0fc52
Rosy <Rosy@Rosy-HP>
2013-12-12 02:56:24 -0200

    Modificacoes Gerais

--- src/java/Presentation/FuncaoController.java
+++ src/java/Presentation/FuncaoController.java
@@ -8,8 +8,15 @@ package Presentation;
 
 import DomainModel.Funcao;
 import DomainModel.IFuncaoRepositorio;
+import java.io.IOException;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -17,7 +24,11 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 //import javax.inject.Named;
+
 
 /**
  *
@@ -56,6 +67,45 @@ public class FuncaoController implements Serializable {
     public void exibirMensagem(String msg) {
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage(msg));
+    }
+    
+    public void XLS(java.awt.event.ActionEvent actionEvent) throws JRException, IOException {
+        Connection conn;
+        String arquivo = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/relatorio/Funcoes Cadastradas.jasper");
+        try {
+
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/TrabalhoFinal [adm em ADM]");
+            java.sql.Statement sql = conn.createStatement();
+            ResultSet rs = sql.executeQuery("     \"ADM\".\"FUNCAO\" FUNCAO" +
+                    "SELECT\n" +
+                    "     FUNCAO.\"IDFUNCAO\" AS FUNCAO_IDFUNCAO,\n" +
+                    "     FUNCAO.\"HORASSEMANAIS\" AS FUNCAO_HORASSEMANAIS,\n" +
+                    "     FUNCAO.\"NOME\" AS FUNCAO_NOME\n" +
+                    "FROM\n");
+            JRDataSource ds = new JRResultSetDataSource(rs);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(arquivo, null, ds);
+
+         ttpServletResponse httpServletResponse = (HttpServletResponse)     FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        httpServletResponse.addHeader("Content-disposition", "attachment; filename= relatorioController.XLS");
+        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+        //JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+        JRXlsExporter xls = new JRXlsExporter();
+        xls.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        xls.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+        xls.exportReport();
+        FacesContext.getCurrentInstance().responseComplete();
+ 
+            
+            
+        } catch (JRException ex) {
+            Logger.getLogger(RelatorioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RelatorioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(RelatorioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     }
 
     public void salvar() {
